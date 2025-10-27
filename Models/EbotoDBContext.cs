e26670eceabd872068a505bc381419d4eb7ad003
Glyzel Galagar <133832079+galazel@users.noreply.github.com>
2025-10-27 21:47:25 +0800

    asfsdfsdf

---
+++ Models/EbotoDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBoto.Models
+{
+    internal class EbotoDBContext : DbContext
+    {
+        public EbotoDBContext() : base("name=EbotoDBContext"){}
+        public DbSet<Candidate> Candidates { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Election> Elections { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Voted> Voted { get; set; }
+        public DbSet<Voter> Voters { get; set; }
+
+
+
+    }
+}
